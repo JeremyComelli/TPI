3be1325fa8a3e16040d3715d474e965c5b583cf9
COMELLI Jeremy <Jeremy.COMELLI@cpnv.ch>
2018-05-31 15:28:44 +0200

    added some neural processing stuff

---
+++ Projet/sources/NN.py
@@ -0,0 +1,85 @@
+import tensorflow as tf
+from tensorflow.examples.tutorials.mnist import input_data
+import pandas
+import os
+import numpy as np
+import matplotlib
+
+
+# Technically it's an interface between my app and tensorflow, but to be honest "Neural Interface" sounds cool
+class NeuralInterface:
+    def __init__(self, in_image_width=0, in_image_height=0):
+        # We disable annoying warnings about soon-to-be-deprecated functions. This isn't exactly a good practice, but since the project will be archived in less than a month, I don't see any problems
+        self.session = tf.InteractiveSession()
+
+        tf.logging.set_verbosity(tf.logging.ERROR)
+        self.run(input_data.read_data_sets("MNIST_data/", one_hot=True))
+
+    @staticmethod
+    def weight_variable(shape):
+        initial = tf.truncated_normal(shape, stddev=0.1)
+        return tf.Variable(initial)
+
+    @staticmethod
+    def bias_variable(shape):
+        initial = tf.constant(0.1, shape=shape)
+        return tf.Variable(initial)
+
+    @staticmethod
+    def conv2d(x, W):
+        return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME')
+
+    @staticmethod
+    def max_pool_2x2(x):
+        return tf.nn.max_pool(x, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
+
+    def run(self, dataset):
+
+        x = tf.placeholder(tf.float32, [None, 784])
+        x_image = tf.reshape(x, [-1,28,28,1])
+
+        W = tf.Variable(tf.zeros([784, 10]))
+        b = tf.Variable(tf.zeros([10]))
+
+        y = tf.nn.softmax(tf.matmul(x, W) + b)
+        y_ = tf.placeholder(tf.float32, [None, 10])
+
+        # Here we define two hidden pooling layers, used to simplify the image to a bunch of vectors
+        W_conv1 = self.weight_variable([5, 5, 1, 32])
+        b_conv1 = self.bias_variable([32])
+
+        h_conv1 = tf.nn.relu(self.conv2d(x_image, W_conv1) + b_conv1)
+        h_pool1 = self.max_pool_2x2(h_conv1)
+
+        W_conv2 = self.weight_variable([5, 5, 32, 64])
+        b_conv2 = self.bias_variable([64])
+
+        h_conv2 = tf.nn.relu(self.conv2d(h_pool1, W_conv2) + b_conv2)
+        h_pool2 = self.max_pool_2x2(h_conv2)
+
+        W_fc1 = self.weight_variable([7 * 7 * 64, 1024])
+        b_fc1 = self.bias_variable([1024])
+
+        h_pool2_flat = tf.reshape(h_pool2, [-1, 7*7*64])
+        h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
+
+        cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits=y))
+
+        train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
+        tf.global_variables_initializer().run()
+        for _ in range(1000):
+            batch = dataset.train.next_batch(100)
+            train_step.run(feed_dict={x: batch[0], y_: batch[1]})
+            # self.session.run(train_step, feed_dict={x: batch_xs, y_: batch_ys})
+
+        correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
+
+        accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+        print(self.session.run(accuracy, feed_dict={x: dataset.test.images, y_: dataset.test.labels}))
+
+
+if __name__ == '__main__':
+    print("Running standalone")
+    Network = NeuralInterface(0, 0)
+
